5c849ef02477541633a9acef1f137f9811ee892b
Bedi Omuri <113227370+Bedi06@users.noreply.github.com>
2024-01-27 11:35:30 +0000

    testing github actions and made changes to the rds pool

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,14 @@ import "./App.css";
 import Header from "./components/Header";
 import AddVideo from "./components/AddVideo";
 import VideoCards from "./components/VideoCards";
-import config from "./config";
 
-const baseUrl = config.REACT_APP_BACKEND_URL;
+const baseUrl = "http://ec2-34-226-208-118.compute-1.amazonaws.com:3000";
 
 function App() {
   const [videos, setVideos] = useState([]);
   const [search, setNewSearch] = useState("");
 
   useEffect(() => {
-    console.log(baseUrl);
     fetch(`${baseUrl}`)
       .then((response) => response.json())
       .then((data) => setVideos(data))
@@ -30,27 +28,43 @@ function App() {
       .then((response) => response.json())
       .then((data) => {
         console.log("Video added successfully with ID:", data.id);
-        console.log("Server Response:", data); // Log the entire response
+        console.log("Server Response:", data);
       })
       .catch((error) => console.error("Error adding video:", error));
   };
 
-  const handleRemove = (id) => {
+  const handleRemove = async (id) => {
     const updatedVideos = videos.filter((video) => video.id !== id);
     setVideos(updatedVideos);
 
-    fetch(`${baseUrl}/${id}`, {
-      method: "DELETE",
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        if (result["result"] !== "failure") {
-          console.log("Video deleted successfully with ID:", id);
-        } else {
-          console.log("Could not delete video");
+    try {
+      const response = await fetch(`${baseUrl}/${id}`, {
+        method: "DELETE",
+      });
+
+      if (!response.ok) {
+        let errorMessage = "Failed to delete video.";
+
+        try {
+          const errorData = await response.json();
+          if (errorData && errorData.message) {
+            errorMessage += ` Server Error: ${errorData.message}`;
+          }
+        } catch (jsonError) {
+          console.error("Error parsing JSON error response:", jsonError);
         }
-      })
-      .catch((error) => console.error("Error deleting video:", error));
+
+        throw new Error(errorMessage);
+      }
+
+      const successData = await response.json(); // Parse success response if available
+      console.log(
+        `Video deleted successfully with ID: ${id}. Server Response:`,
+        successData
+      );
+    } catch (error) {
+      console.error("Error deleting video:", error.message);
+    }
   };
 
   const handleSearch = (event) => {
